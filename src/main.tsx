a703e19e35068aab8a9e3479d10b681c476bf736
leoguilbor <leoguilbor@gmail.com>
2022-12-03 13:24:17 -0300

    2 files modified

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,34 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-function insertIntoTarget(element: any, options: any) {
-  var parent = options || document.head;
+function insertIntoTarget(element: Node) {
+  var parent = document.head;
   console.log('teste');
   parent.appendChild(element);
 }
+class createCustomElement extends HTMLElement {
+  connectedCallback() {
+    const mountPoint = document.createElement('span');
+    const shadowRoot: ShadowRoot = mountPoint.attachShadow({ mode: 'open' });
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    const body = document.createElement('body');
+    shadowRoot.appendChild(body);
+
+    const root = ReactDOM.createRoot(body);
+    root.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+  }
+}
+
+const el = document.createElement('style');
+insertIntoTarget(el);
+
+customElements.define('createCustomElement', createCustomElement);
+/*ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+);*/
