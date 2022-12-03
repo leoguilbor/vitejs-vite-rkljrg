12c1e53bbe738154a880154df94d447a45b8eb71
leoguilbor <leoguilbor@gmail.com>
2022-12-03 12:00:57 -0300

    1 file modified

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+function insertIntoTarget(element: any, options: any) {
+  var parent = options || document.head;
+  console.log('teste');
+  parent.appendChild(element);
+}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
